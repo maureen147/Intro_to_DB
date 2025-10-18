f3e1e85f8ecbd114537c1f8305012c7e177b9b3f
glowworm147 <maureen.attah@hermextravels.com>
2025-10-18 01:38:11 +0100

    Fix task 4 - exact pattern matching for automated checks

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,3 @@
-SELECT * FROM information_schema.columns 
-WHERE table_schema = 'alx_book_store' AND table_name = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
